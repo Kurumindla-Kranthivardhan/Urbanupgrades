5e4af3584ebac57523a9d4da587da7b57327c2fb
Kranthi09 <75693825+Kurumindla-Kranthivardhan@users.noreply.github.com>
2023-11-03 11:12:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { API, Auth } from 'aws-amplify';
 
 function App() {
+  const [devices, setDevices] = useState([]);
+  const [selectedDevice, setSelectedDevice] = useState('');
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetchDevices();
+    fetchUser();
+  }, []);
+
+  async function fetchDevices() {
+    // Modify this query according to your updated GraphQL schema
+    const apiData = await API.graphql({
+      query: customListDevices, // Replace with your query name
+    });
+    const devicesFromAPI = apiData.data.customListDevices.items; // Replace with the query response field
+    setDevices(devicesFromAPI);
+  }
+
+  async function fetchUser() {
+    const authUser = await Auth.currentAuthenticatedUser();
+    setUser(authUser);
+  }
+
+  const handleDeviceChange = (event) => {
+    setSelectedDevice(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
+      <h1>Welcome, {user ? user.username : 'Guest'}</h1>
+      <h2>Select a Device:</h2>
+      <select value={selectedDevice} onChange={handleDeviceChange}>
+        <option value="">Select a device</option>
+        {devices.map((device) => (
+          <option key={device.id} value={device.id}>
+            {device.name}
+          </option>
+        ))}
+      </select>
+      {selectedDevice && (
+        <div>
+          <h2>Device Details:</h2>
+          <p>Name: {devices.find((device) => device.id === selectedDevice)?.name}</p>
+          <p>Type: {devices.find((device) => device.id === selectedDevice)?.type}</p>
+          <p>Status: {devices.find((device) => device.id === selectedDevice)?.status}</p>
+        </div>
+      )}
     </div>
   );
 }
